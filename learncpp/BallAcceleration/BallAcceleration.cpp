0e6eeffaecd302c0262567115a761b9897bde122
Muhammad Rafay <123486185+mrafay-dev@users.noreply.github.com>
2025-12-12 20:25:26 +0000

    Create BallAcceleration.cpp

---
+++ learncpp/BallAcceleration/BallAcceleration.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "BallAccelerationFunctions.h"
+
+
+
+int main(){
+	
+	std::cout << "At time " << getTime(getHeight()) << " seconds, the ball is on the ground \n";
+	
+	system("pause");
+	return 0;
+}
