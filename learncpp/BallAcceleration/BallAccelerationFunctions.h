88defb9e834df7c3a22cb31dcdad91b275f2e88b
Muhammad Rafay <123486185+mrafay-dev@users.noreply.github.com>
2025-12-12 20:26:03 +0000

    Create BallAccelerationFunctions.h

---
+++ learncpp/BallAcceleration/BallAccelerationFunctions.h
@@ -0,0 +1,12 @@
+#ifndef BALLACCELERATIONFUNCTIONS_h
+#define BALLACCELERATIONFUNCTIONS_h
+
+
+
+double getNewHeight(float time, double height);
+
+int getTime(double height);
+
+double getHeight();
+
+#endif
